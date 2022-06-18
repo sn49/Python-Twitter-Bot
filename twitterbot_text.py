a6727b49e58dec81c7a26fef4ccc5ac5aca9412a
sn49 <34567701+sn49@users.noreply.github.com>
2022-06-18 17:20:12 +0900

    키워드 버전 갱신 오류 수정

--- twitterbot_text.py
+++ twitterbot_text.py
@@ -12,10 +12,15 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-version = {"keyword_update": 0, "feature_update": 2, "error_fix": 2}
+version = {"keyword_update": 0, "feature_update": 2, "error_fix": 3}
 show_version = f"V=k{version['keyword_update']}-f{version['feature_update']}-e{version['error_fix']}"
 
 keyword_list = []
+
+
+def update_sv():
+    global show_version
+    show_version = f"V=k{version['keyword_update']}-f{version['feature_update']}-e{version['error_fix']}"
 
 
 def twitter_up(descript):
@@ -39,6 +44,8 @@ def update_wordlist():
             one_word = line.replace("\n", "")
             if not one_word == "":
                 keyword_list.append(one_word)
+            print(keyword_list)
+    update_sv()
     twitter_up(
         f"홀수시에 아무거나 대결 시키는 계정 | 비주기적으로 키워드 추가 | 현재 키워드 : {len(keyword_list)}개 | 버전 : {show_version}"
     )
