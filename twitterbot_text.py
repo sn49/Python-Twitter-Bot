1bb5697132971cc4865a7142763cddb312880d06
sn49 <34567701+sn49@users.noreply.github.com>
2022-06-18 17:08:14 +0900

    키워드 자동 업데이트, 1일 6대결 > 1일 12대결

--- twitterbot_text.py
+++ twitterbot_text.py
@@ -1,3 +1,4 @@
+import os
 import tweepy
 from time import sleep
 import datetime
@@ -11,29 +12,42 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-version = {"keyword_update": 0, "feature_update": 1, "error_fix": 2}
-
+version = {"keyword_update": 0, "feature_update": 2, "error_fix": 2}
 
 keyword_list = []
 
-my_file = open("keyword.kwd", "r", encoding="UTF-8")
-file_lines = my_file.readlines()
-my_file.close()
 
-# Create a for loop to iterate over file_lines
-for line in file_lines:
-    if "version=" in line:
-        line = line.replace("\n", "")
-        version["keyword_update"] = int(line.split("=")[1])
-    else:
-        keyword_list.append(line.replace("\n", ""))
+def update_wordlist():
+    global version
+    global keyword_list
+    keyword_list.clear()
 
-print(keyword_list)
+    my_file = open("keyword.kwd", "r", encoding="UTF-8")
+    file_lines = my_file.readlines()
+    my_file.close()
+
+    for line in file_lines:
+        if "version=" in line:
+            line = line.replace("\n", "")
+            version["keyword_update"] = int(line.split("=")[1])
+        else:
+            one_word = line.replace("\n", "")
+            if not one_word == "":
+                keyword_list.append(one_word)
+    twitter_up(
+        f"홀수시에 아무거나 대결 시키는 계정 | 비주기적으로 키워드 추가 | 현재 키워드 : {len(keyword_list)}개 | 버전 : {show_version}"
+    )
+
+
+update_wordlist()
+
+
 show_version = f"V=k{version['keyword_update']}-f{version['feature_update']}-e{version['error_fix']}"
 
-api.update_profile(
-    description=f"1시,5시,9시,13시,17시,21시 아무거나 대결 시키는 계정 | 비주기적으로 키워드 추가 | 현재 키워드 : {len(keyword_list)}개 | 버전 : {show_version}"
-)
+
+def twitter_up(descript):
+    api.update_profile(description=descript)
+
 
 while True:
     # Add try ... except block to catch and output errors
@@ -42,11 +56,14 @@ while True:
         cmin = cnow.minute
         chour = cnow.hour
 
-        if cmin == 0 and chour % 4 == 1:
+        if cmin == 0 and chour % 2 == 1:
             select_keyword = random.sample(keyword_list, 2)
 
             api.update_status(f"{select_keyword[0]} VS {select_keyword[1]}")
         else:
+            if os.path.exists("goupdate.txt"):
+                if open("goupdate.txt", "r").read() == "Update WordList":
+                    update_wordlist()
             print(f"{chour}시 {cmin}분")
 
     except tweepy.TwitterServerError as e:
